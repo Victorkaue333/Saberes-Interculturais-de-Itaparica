501d8ce3a89f396bfe3a89458add20b95a2dd2ef
Victorkaue333 <kaue.alves.pg@gmail.com>
2026-01-19 10:33:09 -0300

    Update VK
    Implementação do js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Rolagem suave ao clicar no menu
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const id = this.getAttribute('href');
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
